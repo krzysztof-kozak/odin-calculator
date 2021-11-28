40fe734db92c78a99a8c00019720182f39208f7d
Kris <krzysztofkoz6@gmail.com>
2021-11-28 21:05:10 +0100

    Refactor order of operations

--- js/operate.js
+++ js/operate.js
@@ -9,80 +9,9 @@ function operate(memory) {
 	*/
 	memory.splice(-1);
 
-	// Let's convert strings to floats.
-	// let [num1, operator, num2] = memory;
-	// num1 = parseFloat(num1, 10);
-	// num2 = parseFloat(num2, 10);
+	const result = calculateAllSubOperations(memory);
 
-	// const result = getResult(num1, num2, operator);
-
-	// return result;
-
-	console.log(memory); // ['5', '+', '2', 'x', '10']
-
-	/*
-
-	For now assume the memory will always be" num1 + num2 x num3
-
-	0. starting memory: ['5', '+', '2', 'x', '10']
-
-	1. Find all operators.
-		+ -> index 1.
-		x -> index 3.
-
-	2. Respect order of operations and calculate.
-		2.1 'x' should go first
-
-		2.2 find elements next to x:
-			'2' -> index x - 1
-			'10' -> index x + 1
-
-		2.3 replace 2, x, 10 with the result 20.
-			updated memory: ['5', '+', 20].
-
-		2.4 '+' should go second
-
-		2.5 find elements next to +:
-			'5' -> index '+' - 1
-			20 -> index '+' + 1
-
-		2.6 replace 5, +, 20 with the result 25.
-			updated memory: [25]
-
-	3. Return result
-
-	*/
-
-	// 1.
-	const timesIndex = memory.indexOf("x");
-	console.log({ timesIndex });
-
-	const multiplicationResult = getResult(
-		parseFloat(memory[timesIndex - 1]),
-		parseFloat(memory[timesIndex + 1]),
-		"x"
-	);
-
-	console.log({ multiplicationResult });
-
-	memory.splice(timesIndex - 1, 3, multiplicationResult);
-	console.log("memory after multiplication operation: " + memory);
-
-	const plusIndex = memory.indexOf("+");
-	console.log({ plusIndex });
-
-	const additionResult = getResult(
-		parseFloat(memory[plusIndex - 1]),
-		parseFloat(memory[plusIndex + 1]),
-		"+"
-	);
-
-	console.log({ additionResult });
-
-	memory.splice(plusIndex - 1, 3, additionResult);
-	console.log("memory after addition operation: " + memory);
-
-	return memory;
+	return result;
 }
 
 function getResult(num1, num2, operator) {
@@ -102,6 +31,46 @@ function getResult(num1, num2, operator) {
 		default:
 			break;
 	}
+}
+
+// [2 + 5 x 10 + 25 - 5 + 20 ÷ 4 x 2 + 1] -> 83
+
+function calculateAllSubOperations(memory) {
+	debugger;
+	// This is a recursive function, see the bottom line.
+	if (memory.length === 1) {
+		return memory[0];
+	}
+
+	// Search for multiplication or division operation first.
+	let operator = memory.find((element) => element === "x" || element === "÷");
+
+	// If not found, search for addition or subtraction operation.
+	if (!operator) {
+		operator = memory.find((element) => element === "+" || element === "-");
+	}
+
+	// Now that we have the idnex of the operator...
+	const operatorIndex = memory.indexOf(operator);
+
+	// ...let's get the numbers to operate on
+	const num1 = parseFloat(memory[operatorIndex - 1]);
+	const num2 = parseFloat(memory[operatorIndex + 1]);
+
+	const operationResult = getResult(num1, num2, operator);
+
+	/* 
+	Now that we have operated, lets update our memory.
+
+	Example:
+
+	starting point: [5 + 2 x 10].
+	end point: [5 + 20].
+	*/
+	memory.splice(operatorIndex - 1, 3, operationResult);
+
+	// Run the function recursively, until all sub-operation are resolved.
+	return calculateAllSubOperations(memory);
 }
 
 export default operate;
