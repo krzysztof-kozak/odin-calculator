7dae246b2f78b2b486d6226f71b60dec83ef7146
Kris <krzysztofkoz6@gmail.com>
2021-12-03 22:18:15 +0100

    Add parenthesis when constructing negative numbers
    For example:
    9 - -5 becomes 9 - (-5)
    9 x -5 becomes 9 x (-5)

--- js/updateDisplay.js
+++ js/updateDisplay.js
@@ -1,3 +1,7 @@
+import { userIputHistory } from "./index.js";
+
+let isConstructingNegativeNumber = false;
+
 function updateDisplay(display, value) {
 	if (typeof value === "object") {
 		value = value.join(" ");
@@ -5,7 +9,27 @@ function updateDisplay(display, value) {
 		return;
 	}
 
+	/* 
+	put negative numbers into parenthesis, e.g.:
+	9 - -5 becomes 9 - (-5)
+	9 x -5 becomes 9 x (-5) 
+	*/
+	const previousInput = userIputHistory.at(-2);
+	if (isOperator(previousInput) && value === "-") {
+		value = `(${value}`;
+		isConstructingNegativeNumber = true;
+	}
+
+	if (isConstructingNegativeNumber && isOperator(value)) {
+		value = `)${value}`;
+		isConstructingNegativeNumber = false;
+	}
+
 	display.textContent += value;
+}
+
+function isOperator(value) {
+	return ["+", "-", "x", "÷"].includes(value);
 }
 
 export default updateDisplay;
