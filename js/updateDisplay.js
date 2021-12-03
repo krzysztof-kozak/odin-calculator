037ba39cffd526579998c1d61e803b45697e8a3f
Kris <krzysztofkoz6@gmail.com>
2021-12-04 00:29:56 +0100

    Display negative numbers in parenthesis on the main display

--- js/updateDisplay.js
+++ js/updateDisplay.js
@@ -3,17 +3,18 @@ import { userIputHistory, memory } from "./index.js";
 let isConstructingNegativeNumber = false;
 
 function updateDisplay(display, value) {
-	if (typeof value === "object") {
-		value = value.join(" ");
-		display.textContent = value;
-		return;
-	}
-
 	/* 
 	put negative numbers into parenthesis, e.g.:
 	9 - -5 becomes 9 - (-5)
 	9 x -5 becomes 9 x (-5) 
 	*/
+	if (typeof value === "object") {
+		value = value.map((el) => (el = isNegative(el) ? `(${el})` : el)).join(" ");
+		display.textContent = value;
+		return;
+	}
+
+	// dynamicaly put negative numbers int parenhesis as they are being constructed
 	const previousInput = userIputHistory.at(-2);
 	if (isOperator(previousInput) && value === "-") {
 		isConstructingNegativeNumber = true;
@@ -43,4 +44,7 @@ function isOperator(value) {
 	return ["+", "-", "x", "÷"].includes(value);
 }
 
+function isNegative(el) {
+	return el.length > 1 && el.includes("-");
+}
 export default updateDisplay;
